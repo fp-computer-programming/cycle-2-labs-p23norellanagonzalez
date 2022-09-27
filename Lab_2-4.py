4ee4723b33168f1ef327fc2f81efad26bd64365e
p23norellanagonzalez <p23norellanagonzalez@fairfieldprep.org>
2022-09-27 10:21:48 -0400

    absent work

---
+++ Lab_2-4.py
@@ -0,0 +1,13 @@
+# author NDO 09/16/22
+a = 1
+b = 2
+c = 3
+x = 2
+y = 5 
+l = (3 + (4*x))/5
+m = 10 * (y-5) * (a + b + c)**2 /x 
+o = ((4/x**2)+ ((9+x)/y)) * 9
+print(l-m+o)
+print(l)
+print(m)
+print(o)
