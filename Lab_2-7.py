c000e7b959722d35e7bb7509e3e01fe4c387e5f0
p23norellanagonzalez <p23norellanagonzalez@fairfieldprep.org>
2022-09-28 10:44:12 -0400

    All absent work has been finished

--- Lab_2-7.py
+++ Lab_2-7.py
@@ -19,9 +19,11 @@ if c % 2 == 0 :
     print ("a is a even number")
 else: 
     print ("a is an odd number")
+if c > -5 or c < 5 :
+    print('a is between -5 and 5')
 if d % 2 == 0 : 
     print ("a is a even number")
 else: 
     print ("a is an odd number")
-if a > -5 or a < 5 :
-    print('a is between -5 and 5')
+if d > -5 or d < 5 :
+    print('a is between -5 and 5')   
--- Lab_2-8.py
+++ Lab_2-8.py
@@ -1,1 +1,17 @@
 # author NDO 09/27/22
+# input a value for x using substituting 15 
+x = 12
+if x >= 15 : 
+    print('the team earned a gold medal') 
+else:
+    if x >=12 or x>= 14 :
+        print('the team earned a silver medal')
+    else :
+            if x>= 9 or x>=11 :
+                print( 'the team earned a bronze medal ')
+            else :
+               if x<=8 :
+                      print( 'the team earned no medal')
+
+     
+      
